c6160aed944edb328c5d0355d3088c5c505655e8
taka2 <taka23131@gmail.com>
2022-08-22 21:09:39 +0900

    evalの完成

--- src/ast.rs
+++ src/ast.rs
@@ -5,14 +5,14 @@ pub enum AST {
     LmApp(Box<AST>, Box<AST>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Term {
     TmVar(Info, usize, usize),
     TmAbs(Info, String, Box<Term>),
     TmApp(Info, Box<Term>, Box<Term>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Info {
     Info,
 }
@@ -96,6 +96,84 @@ impl Term {
                 new_ctx.push(x_mut.clone());
                 return (new_ctx, x_mut);
             }
+        }
+    }
+
+    fn shift(&self, d: i64) -> Term {
+        self.shift_walk(d, 0)
+    }
+
+    fn shift_walk(&self, d: i64, c: usize) -> Term {
+        match self {
+            Term::TmVar(fi, x, n) => {
+                if *x >= c {
+                    Term::TmVar(
+                        fi.clone(),
+                        (*x as i64 + d) as usize,
+                        (*n as i64 + d) as usize,
+                    )
+                } else {
+                    Term::TmVar(fi.clone(), *x, (*n as i64 + d) as usize)
+                }
+            }
+            Term::TmAbs(fi, x, t1) => {
+                Term::TmAbs(fi.clone(), x.clone(), Box::new(t1.shift_walk(d, c + 1)))
+            }
+
+            Term::TmApp(fi, t1, t2) => Term::TmApp(
+                fi.clone(),
+                Box::new(t1.shift_walk(d, c)),
+                Box::new(t2.shift_walk(d, c)),
+            ),
+        }
+    }
+
+    fn subst(&self, j: usize, s: Term) -> Term {
+        self.subst_walk(j, s, 0)
+    }
+
+    fn subst_walk(&self, j: usize, s: Term, c: usize) -> Term {
+        match self {
+            Term::TmVar(fi, x, n) => {
+                if *x == j + c {
+                    s.shift(c as i64)
+                } else {
+                    Term::TmVar(fi.clone(), *x, *n)
+                }
+            }
+            Term::TmAbs(fi, x, t1) => {
+                Term::TmAbs(fi.clone(), x.clone(), Box::new(t1.subst_walk(j, s, c + 1)))
+            }
+            Term::TmApp(fi, t1, t2) => Term::TmApp(
+                fi.clone(),
+                Box::new(t1.subst_walk(j, s.clone(), c)),
+                Box::new(t2.subst_walk(j, s, c)),
+            ),
+        }
+    }
+
+    fn subst_top(&self, s: Term) -> Term {
+        self.subst(0, s.shift(1)).shift(-1)
+    }
+
+    fn isval(&self, ctx: &Context) -> bool {
+        match self {
+            Term::TmAbs(_, _, _) => true,
+            _ => false,
+        }
+    }
+
+    pub fn eval(&self, ctx: &Context) -> Term {
+        match self {
+            Term::TmApp(fi, t1, t2) => {
+                let v1 = t1.eval(ctx);
+                let v2 = t2.eval(ctx);
+                match v1 {
+                    Term::TmAbs(_, x, t12) => t12.subst_top(v2).eval(ctx),
+                    t => Term::TmApp(fi.clone(), Box::new(t), Box::new(v2)),
+                }
+            }
+            t => t.clone(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 #[macro_use]
 extern crate lalrpop_util;
 mod ast;
+use std::io::{self, Write};
 
 lalrpop_mod!(pub parser); // synthesized by LALRPOP
 
+macro_rules! print_flush {
+    ( $( $x:expr ),* ) => {
+        print!( $($x, )* );
+
+        std::io::stdout().flush().expect("Could not flush to standard output.");
+    };
+}
+
 fn main() {
-    parser::TopParser::new()
-        .parse(r"\w. \a. x")
-        .unwrap()
-        .into_term()
-        .printtm(&vec![
-            "x".to_string(),
-            "y".to_string(),
-            "z".to_string(),
-            "a".to_string(),
-            "b".to_string(),
-        ])
+    repl()
+}
+
+fn repl() {
+    let parser = parser::TopParser::new();
+    loop {
+        println!();
+        print_flush!("untyped?> ");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could not read from standard input.");
+
+        if &input == ":q\n" || &input == ":quit\n" {
+            break;
+        } else if input.chars().all(char::is_whitespace) {
+            continue;
+        }
+
+        parser
+            .parse(&input)
+            .unwrap()
+            .into_term()
+            .eval(&vec![
+                "x".to_string(),
+                "y".to_string(),
+                "z".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+            ])
+            .printtm(&vec![
+                "x".to_string(),
+                "y".to_string(),
+                "z".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+            ])
+    }
 }
